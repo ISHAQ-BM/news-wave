a00004ef706154940cf9dc3c8cad5811663fa6dc
ISHAQ-BM <isaacbelhadjmehdi@gmail.com>
2023-11-17 02:09:56 +0100

    article frag

--- app/src/main/java/com/example/newswave/presentation/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/newswave/presentation/ui/fragments/ArticleFragment.kt
@@ -5,11 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.newswave.R
+import com.example.newswave.databinding.FragmentArticleBinding
+import com.example.newswave.domain.models.Article
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ArticleFragment : Fragment() {
+
+    private var binding: FragmentArticleBinding? = null
+    private val args: ArticleFragmentArgs by navArgs()
 
 
     override fun onCreateView(
@@ -17,7 +25,25 @@ class ArticleFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_article, container, false)
+        binding=FragmentArticleBinding.inflate(inflater,container,false)
+        return binding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val url= args.articleLink
+        binding?.webView?.webViewClient= WebViewClient()
+        binding?.webView?.loadUrl(url)
+
+        binding?.navigatePrev?.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding = null
+
     }
 
 
