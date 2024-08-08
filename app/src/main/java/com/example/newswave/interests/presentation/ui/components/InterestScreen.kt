7da9b0c8c5a4f047733c61affedb3bc88765d4d6
ISHAQ-BM <isaacbelhadjmehdi@gmail.com>
2024-08-08 18:14:20 +0100

    Update Interests screen UI: chips replaced with switches

--- app/src/main/java/com/example/newswave/interests/presentation/ui/components/InterestScreen.kt
+++ app/src/main/java/com/example/newswave/interests/presentation/ui/components/InterestScreen.kt
@@ -6,16 +6,24 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
@@ -24,24 +32,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontVariation.weight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.example.newswave.R
 import com.example.newswave.core.presentation.ui.components.LoadingButton
 import com.example.newswave.core.util.categories
 import com.example.newswave.interests.presentation.viewmodel.InterestsViewModel
 
 
-@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun InterestScreen(
     modifier: Modifier=Modifier,
     viewModel: InterestsViewModel = hiltViewModel(),
-    onSaveSuccess :()->Unit
+    onSaveSuccess :(Boolean?)->Unit,
+    isNewUser:Boolean?=false
 
 ){
+    Log.d("intr ui state","${viewModel.uiState}")
     Scaffold(
         topBar = {
             @OptIn(ExperimentalMaterial3Api::class)
@@ -62,53 +75,81 @@ fun InterestScreen(
                 .fillMaxSize()
         ) {
             val uiState by viewModel.uiState.collectAsState()
-            Log.d("interests ui","${uiState.interestsList}")
-            onSaveSuccess()
+            if (uiState.updateSuccessful){
+                onSaveSuccess(isNewUser)
+            }
 
 
-            FlowRow(
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                horizontalArrangement = Arrangement.spacedBy(24.dp),
-                modifier = modifier
-                    .fillMaxWidth()
-                    .weight(1f)
 
-            ) {
-                categories.forEach { it ->
-                    InterestItem(label = it, isSelected =uiState.interestsList.contains(it) ) { interest ->
-                        viewModel.interestClicked(interest)
+
+
+                LazyColumn (
+                    modifier = Modifier.weight(1f)
+                ){
+                    items(categories) {item->
+                        InterestItem(label = item, isChecked = uiState.interestsList.contains(item),
+                            onCheck = { interest -> viewModel.interestClicked(interest) })
                     }
                 }
-            }
+                Button(
+                    onClick = { viewModel.updateInterests() },
+                    modifier=modifier.fillMaxWidth().height(48.dp),
+                    enabled = uiState.previousInterests.toSet() != uiState.interestsList.toSet()
+                ) {
+                    Box (
+                        contentAlignment = Alignment.Center
+                    ){
+                        if (uiState.isLoading && uiState.previousInterests.toSet() != uiState.interestsList.toSet())
+                            CircularProgressIndicator(
+                                color = Color.White,
+                                modifier = Modifier.size(28.dp)
+                            )
+                        else
+                            Text(text = "Save")
+                    }
+
+                }
 
 
-            Button(
-                onClick = { viewModel.updateInterests() },
-                modifier=modifier.fillMaxWidth(),
-                enabled = uiState.previousInterests.toSet() != uiState.interestsList.toSet()
-            ) {
-                Text(text = "Save")
-            }
+
         }
     }
 }
 
+
+
 @Composable
 fun InterestItem(
+    modifier: Modifier=Modifier,
     label:String,
-    isSelected:Boolean ,
-    onClick : (String)->Unit
+    isChecked:Boolean,
+    onCheck:(String)->Unit
 ){
-    var selected by remember(key1 = isSelected) { mutableStateOf(isSelected) }
-    FilterChip(
-        onClick = {
-            selected = !selected
-            onClick(label)
-                  },
-        label = {
-            Text(label)
-        },
-        selected = selected,
-    )
+    var checked by remember(key1 = isChecked) { mutableStateOf(isChecked) }
+
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(vertical = 12.dp)
+
+    ){
+        Text(
+            text = label,
+            modifier
+                .weight(1f)
+                .padding(start = 8.dp)
+        )
+        Switch(
+            checked = checked,
+            onCheckedChange = {
+                checked= !checked
+                onCheck(label)
+            }
+
+        )
+
+
+    }
 }
 
