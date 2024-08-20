d9230b91a80afc6cfc2e1205d6b9f9e28b71ea74
ISHAQ-BM <isaacbelhadjmehdi@gmail.com>
2024-08-20 07:42:27 +0100

    add the searchview to the searchbar

--- app/src/main/java/com/example/newswave/search/ui/SearchScreen.kt
+++ app/src/main/java/com/example/newswave/search/ui/SearchScreen.kt
@@ -13,10 +13,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Clear
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.CircularProgressIndicator
@@ -28,8 +27,8 @@ import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SearchBar
+import androidx.compose.material3.SearchBarDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
@@ -58,6 +57,7 @@ import coil.compose.AsyncImage
 import com.example.newswave.R
 import com.example.newswave.core.presentation.ui.components.PagingNewsList
 import com.example.newswave.core.presentation.ui.state.NewsItemUiState
+import com.example.newswave.core.presentation.ui.theme.NewsWaveTheme
 
 
 @Composable
@@ -83,6 +83,7 @@ fun SearchRoute(
     )
 }
 
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchScreen(
@@ -106,105 +107,105 @@ fun SearchScreen(
     }
     val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior(rememberTopAppBarState())
 
+    NewsWaveTheme {
 
-    Scaffold(
-        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {
-            @OptIn(ExperimentalMaterial3Api::class)
-            TopAppBar(
-                colors = TopAppBarDefaults.topAppBarColors(
-                    containerColor = MaterialTheme.colorScheme.background,
-                    titleContentColor = MaterialTheme.colorScheme.onBackground,
-                ),
 
-                title = {
-                    SearchBar(
-                        query = searchQuery,
-                        onQueryChange = { searchQuery = it },
-                        onSearch = { onSearch(searchQuery) },
-                        active = isSearching,
-                        onActiveChange = {
-                            isSearching = !it
-                        },
-                        placeholder = {
-                            Text(text = stringResource(R.string.search_news))
-                        },
-                        leadingIcon = {
+        Scaffold(
+            modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+            topBar = {
+                @OptIn(ExperimentalMaterial3Api::class)
+                SearchBar(
+                    query = searchQuery,
+                    onQueryChange = { searchQuery = it },
+                    onSearch = { onSearch(searchQuery) },
+                    active = isSearching,
+                    onActiveChange = {
+                        isSearching = it
+                    },
+                    placeholder = {
+                        Text(text = stringResource(R.string.search_news))
+                    },
+                    leadingIcon = {
+                        if (isSearching)
+                            IconButton(onClick = { isSearching = false }) {
+                                Icon(
+                                    imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                                    contentDescription = null
+                                )
+                            }
+                        else
                             Icon(
                                 imageVector = Icons.Default.Search,
                                 tint = MaterialTheme.colorScheme.onSurface,
                                 contentDescription = null
                             )
-                        },
-                        trailingIcon = {
-                            if (!searchQuery.isNullOrEmpty())
-                                IconButton(onClick = {
-                                    searchQuery = ""
-                                }) {
-                                    Icon(
-                                        imageVector = Icons.Default.Clear,
-                                        tint = MaterialTheme.colorScheme.onSurface,
-                                        contentDescription = null
-                                    )
-                                }
-                        },
-                        modifier = modifier
-                            .fillMaxWidth()
-                            .padding(end = 16.dp)
-                    ) {
+                    },
+                    trailingIcon = {
+                        if (!searchQuery.isNullOrEmpty())
+                            IconButton(onClick = {
+                                searchQuery = ""
+                            }) {
+                                Icon(
+                                    imageVector = Icons.Default.Clear,
+                                    tint = MaterialTheme.colorScheme.onSurface,
+                                    contentDescription = null
+                                )
+                            }
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = if (isSearching) 0.dp else 16.dp),
+                    colors = SearchBarDefaults.colors(
+                        containerColor = if (isSearching) MaterialTheme.colorScheme.background else MaterialTheme.colorScheme.surfaceContainerHigh,
+                    )
 
-                    }
-                },
-                scrollBehavior = scrollBehavior
-            )
-
-        }
-    ) {
-        Box(
-            modifier = modifier
-                .padding(top = it.calculateTopPadding())
-        ) {
-            if (searchQuery.isNullOrEmpty()) {
-                Column(
-                    modifier = modifier.verticalScroll(rememberScrollState())
                 ) {
 
-                    Spacer(modifier = modifier.height(16.dp))
 
-                    Text(
-                        text = stringResource(id = R.string.latest_news),
-                        style = MaterialTheme.typography.titleLarge,
-                        fontWeight = FontWeight.Bold,
-                        modifier = modifier.padding(horizontal = 24.dp)
-                    )
-                    Spacer(modifier = modifier.height(24.dp))
-                    LatestNewsList(
-                        latestNews = uiState.latestNews,
-                        onClickNews = onClickNews,
-                        onClickBookmark = onClickBookmark,
-                        onShareNews = onShareNews
-                    )
+                    if (uiState.isLoading) {
+                        CircularProgressIndicator()
+                    } else {
+                        PagingNewsList(
+                            newsList = searchResult,
+                            onClickNews = onClickNews,
+                            onClickBookmark = onClickBookmark,
+                            onShareNews = onShareNews
+                        )
+                    }
+
 
                 }
-            } else {
-                if (uiState.isLoading) {
-                    CircularProgressIndicator()
-                } else {
-                    PagingNewsList(
-                        newsList = searchResult,
-                        onClickNews = onClickNews,
-                        onClickBookmark = onClickBookmark,
-                        onShareNews = onShareNews
-                    )
-                }
+
+            }
+        ) {
+
+            Column(
+                modifier = modifier.padding(top = it.calculateTopPadding())
+            ) {
+
+                Spacer(modifier = modifier.height(16.dp))
+
+                Text(
+                    text = stringResource(id = R.string.latest_news),
+                    style = MaterialTheme.typography.titleLarge,
+                    fontWeight = FontWeight.Bold,
+                    modifier = modifier.padding(horizontal = 24.dp)
+                )
+                Spacer(modifier = Modifier.height(24.dp))
+                LatestNewsList(
+                    latestNews = uiState.latestNews,
+                    onClickNews = onClickNews,
+                    onClickBookmark = onClickBookmark,
+                    onShareNews = onShareNews
+                )
 
             }
 
 
         }
 
-
     }
+
 }
 
 @Composable
